b5e13b0a3113be64e5ff3c4562c18f0e4619da51
Jirattikal42 <60840444+Jirattikal42@users.noreply.github.com>
2020-08-23 16:37:23 +0700

    section 4

--- components/Weather.js
+++ components/Weather.js
@@ -4,24 +4,27 @@ import Forecast from './Forecast';
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
-        main: '-',
-        description: '-',
+        main: 'main',
+        description: 'description',
         temp: 0
     }) 
 
     return (
         <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-          <Text>Zip Code</Text>
-          <Text>{props.zipCode}</Text>
+          <Text>Zip Code is {props.zipCode} </Text>
+        
             <Forecast {...forecastInfo} />
         </ImageBackground> 
     )
 }
 const styles = StyleSheet.create({
     backdrop: {
-        alignItems: 'center',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
         width: '100%',
         height: '100%'
+        
     },
 });
    
