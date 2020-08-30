da5c6d4e95ef2c31e38a658446d92d66cbc064e9
Jirattikal42 <60840444+Jirattikal42@users.noreply.github.com>
2020-08-31 02:54:31 +0700

    commit 4

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { FlatList, View, Text, StyleSheet } from 'react-native'
+import { FlatList, View, Text, StyleSheet, ImageBackground } from 'react-native'
 import { TouchableHighlight } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
-
+import { StatusBar } from 'expo-status-bar'
 
 const availableZipItems = [
     { place: 'Hatyai', code: '90110' },
@@ -19,8 +19,8 @@ const ZipItem = ({place, code, navigation}) => (
     }}>
         
         <View style={styles.zipItem}>
-            <Text>{place}</Text>
-            <Text>{code}</Text>
+            <Text style={styles.zipPlace}>{place}</Text>
+            <Text style={styles.zipCode}>{code}</Text>
         </View>
     </TouchableHighlight>
 )
@@ -30,31 +30,49 @@ const _keyExtractor = item => item.code
 export default function ZipCodeScreen(){
     const navigation = useNavigation()
     return (
-       
-            <FlatList
-                data = {availableZipItems}
-                keyExtractor = {_keyExtractor}
-                renderItem={({item}) => <ZipItem {...item} navigation={navigation} />}
-            />
-            
+        <ImageBackground source={require('../bgh.png')} style={styles.backdrop}>
+            <View>
+                <FlatList
+                    data = {availableZipItems}
+                    keyExtractor = {_keyExtractor}
+                    renderItem={({item}) => <ZipItem {...item} navigation={navigation} />}
+                />
+                <StatusBar style="auto" />
+            </View>
+        </ImageBackground>    
     )
 }
 
 const styles = StyleSheet.create({
     zipItem: {
-        flex: 1,
-        flexDirection: 'row',
+        backgroundColor: 'rgba(125,125,125,0.25)',
+        borderRadius: 20,
+        marginTop: 40,
+        marginBottom: 30,
+        padding: 10,
         justifyContent:'center'
 
     },
     zipPlace: {
         flex: 1,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        color:'#FFFFFF'
         
         
        
     },
     zipCode: {
-        flex: 1
+        flex: 1,
+        textAlign: 'center',
         
+    },
+    backdrop: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
     }
 })
